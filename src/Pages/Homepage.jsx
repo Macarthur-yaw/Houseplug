d4bc18d66f48c4a8c665f0d1544fa614d6d61649
Kevin <arthurkevin1260@gmail.com>
2023-07-02 23:56:27 +0100

    Almost done with the ui

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -1,6 +1,7 @@
 import Navbar from "../Components/Navbar";
 import BackgroundPicture from "../assets/Background.jpg";
 import Pictures from "./Pictures";
+
 const Homepage = () => {
     return (
         
@@ -25,25 +26,70 @@ alt="a background"/>
         </div>
         <div className="relative mt-95">
 
-               <h1 className="text-l font-bold text-center"> All our room types including complementary breakfast.
-               </h1> <div className="flex flex-col gap-2  border-2">   
+               <h1 className="text-l font-bold text-center text-blue-900"> All our room types including complementary breakfast.
+               </h1> <div className="flex  flex-col gap-2  border-2">   
      {Pictures.map((picture, index) => {
   return (
-    <div key={index} className="flex flex-row">
+    <div key={index} className="flex flex-row p-2   mx-auto">
 
-        <div className="flex flex-col">
-   <h1 className="text-l font-bold p-2"> {picture.title}
+        <div className="flex flex-col text-left gap-2 text-blue-900 p-4">
+   <h1 className="text-l font-bold  p-2"> {picture.title}
    </h1>
   
     {picture.content}
+
+    <br/>
+    <button className="border-2 w-32 p-1 border-orange-400 rounded bg-orange-400 text-white">Explore</button>
 </div>
-    <img src={picture.img} alt="an "/>
+    <img src={picture.img} alt="an " width="50%" height="50%"/>
     <br/>
     </div>
   );
 })}
 
 
+</div>
+
+<div className="flex flex-col text-center p-2 text-blue-900" ><h1 className="text-blue-900 text-l font-bold">Testimonials</h1>
+&quot; Calm,Serene, Retro- What a way to relax and enjoy&quot;
+<br/>
+
+Mr and Mrs Baxter, UK.
+
+</div>
+
+<div id="footer" className="flex  flex-row border-2 gap-6 p-4 items-center justify-between font-semibold text-white border-blue-900 p-2 bg-blue-900">
+        <div id="list1" className="flex flex-col gap-2 items-center">
+
+               <h1> HOSTELPLUG</h1>
+               407 Tarkwa Road,
+               +233 599 293 572
+               arthurkevin1260@gmail.com
+        </div>
+
+        <div id="list2" className="flex flex-col text-center items-center">
+                <ul>
+                        <li>About</li>
+                        <li>Contact</li>
+                        <li>Terms & Condition</li>
+                </ul>
+        </div>
+
+        <div id="list3" className="flex flex-col text-center items-center">
+        <ul>
+                        <li>Facebook</li>
+                        <li>Twitter</li>
+                        <li>Instagram</li>
+                </ul>
+
+        </div>
+
+        <div id="list4" className="flex flex-col items-center gap-2 ">
+                Subscribe to our newsletter
+
+         <span className="border-2 border-orange-300 flex  flex-row">       <input type="text" placeholder="Email address" className="border-none bg-transparent placeholder-white text-center"/>
+     <button className="bg-orange-300 text-center ">OK</button>
+     </span>     </div>
 </div>
         </div>
         </>    );
--- src/Pages/Pictures.jsx
+++ src/Pages/Pictures.jsx
@@ -3,13 +3,13 @@ import Beach from "../assets/Beach.jpg";
 
 const Pictures=[
 {
-title:"Luxury redefined",
-content:"Spend time with loved ones using this apartment",
+title:"Luxury redefined.",
+content:"Our rooms are designed to transport you into an environment made for leisure. Take your mind off the day-to-day of home life and find a private paradise for yourself.",
 img:House
 },
 {
-    title:"Leave your worries in the sand",
-    content:"Spend time with loved ones using this apartment",
+    title:"Leave your worries in the sand.",
+    content:"We love life at the beach. Being close to the ocean with access to endless sandybeach ensures a relaxed state of mind. It    seems like time stands still watching the ocean. ",
     img:Beach
 }
 ]
